7c846f936929eecd0258c9e4c7d38f3c51b3b354
john layton <johnstewartlayton@gmail.com>
2022-04-27 17:02:10 +1000

    remove some modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,18 +2,31 @@ pluginManagement {
     repositories {
         maven("https://repo.spring.io/release")
         maven("https://repo.spring.io/snapshot")
+        maven("https://repo.spring.io/milestone")
         maven("https://plugins.gradle.org/m2")
+        gradlePluginPortal()
     }
 }
+
+//pluginManagement {
+//    repositories {
+//        maven { url 'https://repo.spring.io/release' }
+//        maven { url 'https://repo.spring.io/milestone' }
+//        maven { url 'https://repo.spring.io/snapshot' }
+//        gradlePluginPortal()
+//    }
+//}
+
 
 rootProject.name = "bulk"
 
 include(
-    "bulk-multi",
-    "bulk-plan:api",
-    "bulk-plan:common",
-    "bulk-plan:engine",
-    "bulk-trace:api",
-    "bulk-stream",
-    "bulk-cloud:template-consumer"
+//    "bulk-multi",
+//    "bulk-plan:api",
+//    "bulk-plan:common",
+//    "bulk-plan:engine",
+//    "bulk-trace:api",
+//    "bulk-stream",
+//    "bulk-cloud:template-consumer",
+    "bulk-graph:simple"
 )
